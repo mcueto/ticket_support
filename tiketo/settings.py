4bfbddf3d26412ae0c4944d8e9c90f99b0a69fb4
mcueto <cueto@live.cl>
2019-01-25 12:23:29 -0300

    Set postgres as database in project settings

--- tiketo/settings.py
+++ tiketo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tiketo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
