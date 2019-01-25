3691cd688bbe63c89a05a4b4abf7b2471347f072
mcueto <cueto@live.cl>
2019-01-25 05:14:09 -0300

    Create Ticket model in tickets app

--- tickets/models.py
+++ tickets/models.py
@@ -1,6 +1,54 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import uuid
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Fields constants
+CODE_FIELD_MAX_LENGTH = 128
+NAME_FIELD_MAX_LENGTH = 128
+
+# Ticket status field choices
+TICKET_STATUS_CHOICES = (
+    ('open', 'open'),
+    ('pending', 'pending'),
+    ('in_process', 'in_process'),
+    ('resolved', 'resolved'),
+    ('closed', 'closed'),
+)
+
+
+class Ticket(models.Model):
+    """
+    BaseProduct model.
+
+    Contains the common fields between Product and ProductVariant models.
+    """
+    unique_id = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False
+    )
+    code = models.CharField(
+        max_length=CODE_FIELD_MAX_LENGTH
+    )
+    title = models.CharField(
+        max_length=NAME_FIELD_MAX_LENGTH
+    )
+    description = models.TextField(
+        blank=True
+    )
+    current_status = models.CharField(
+        max_length=CODE_FIELD_MAX_LENGTH,
+        choices=TICKET_STATUS_CHOICES
+    )
+    created_at = models.DateTimeField(
+        default=timezone.now
+    )
+    updated_at = models.DateTimeField(
+        default=timezone.now
+    )
+
+    def __str__(self):
+        """Return the class instance item name in django admin."""
+        return str(self.name)+" - "+str(self.created_at)
